250f57e418a59d8adb2e09d9e0e8b1c381a4c10e
AquaBob-15 <90625650+llivshits@users.noreply.github.com>
2025-11-10 23:34:14 -0600

    Fixed version mismatch between package.json and cargo.toml

---
+++ chat-server/target/debug/deps/chat_server-e439ddc0847f1289.d
@@ -0,0 +1,9 @@
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/chat_server-e439ddc0847f1289.d: src/lib.rs src/server.rs src/io_handler.rs src/client_backend.rs src/client_frontend.rs
+
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/libchat_server-e439ddc0847f1289.rmeta: src/lib.rs src/server.rs src/io_handler.rs src/client_backend.rs src/client_frontend.rs
+
+src/lib.rs:
+src/server.rs:
+src/io_handler.rs:
+src/client_backend.rs:
+src/client_frontend.rs:
---
+++ chat-server/target/debug/deps/chat_server-ed2bfe3bb0940ff2.d
@@ -0,0 +1,6 @@
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/chat_server-ed2bfe3bb0940ff2.d: src/main.rs src/io_handler.rs
+
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/libchat_server-ed2bfe3bb0940ff2.rmeta: src/main.rs src/io_handler.rs
+
+src/main.rs:
+src/io_handler.rs:
