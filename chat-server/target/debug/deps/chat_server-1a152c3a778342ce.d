cd4a81e9974fd63e3c107ff70a4d02356d427d69
AquaBob-15 <90625650+llivshits@users.noreply.github.com>
2025-11-10 20:38:29 -0600

    Fixed git ignore

--- chat-server/target/debug/deps/chat_server-1a152c3a778342ce.d
+++ chat-server/target/debug/deps/chat_server-1a152c3a778342ce.d
@@ -1,5 +1,5 @@
-/Users/ryanzheng/Documents/CS 128 Intro to CS II/Honors/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/chat_server-1a152c3a778342ce.d: src/main.rs
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/chat_server-1a152c3a778342ce.d: src/main.rs
 
-/Users/ryanzheng/Documents/CS 128 Intro to CS II/Honors/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/libchat_server-1a152c3a778342ce.rmeta: src/main.rs
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/libchat_server-1a152c3a778342ce.rmeta: src/main.rs
 
 src/main.rs:
--- chat-server/target/debug/deps/chat_server-fcc5eadd4646f497.d
+++ chat-server/target/debug/deps/chat_server-fcc5eadd4646f497.d
@@ -1,5 +1,5 @@
-/Users/ryanzheng/Documents/CS 128 Intro to CS II/Honors/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/chat_server-fcc5eadd4646f497.d: src/main.rs
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/chat_server-fcc5eadd4646f497.d: src/main.rs
 
-/Users/ryanzheng/Documents/CS 128 Intro to CS II/Honors/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/libchat_server-fcc5eadd4646f497.rmeta: src/main.rs
+/Users/lenlivshits/cs199/CS199-128-Final-Project-FA25-/chat-server/target/debug/deps/libchat_server-fcc5eadd4646f497.rmeta: src/main.rs
 
 src/main.rs:
